4ec13cededbe0a5df5192721c118a5d790ce1743
Brigilets <b.sabutyte@gmail.com>
2023-08-02 11:19:14 +0200

    recreated the logic of custom observable without using interval utility function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { Subscription, interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +11,22 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() {}
 
   ngOnInit() {
-    this.firstObservableSubscription = interval(1000).subscribe((count) => {
-      console.log(count);
+    // this.firstObservableSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = new Observable((observer) => {
+      let count: number = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
     });
+
+    this.firstObservableSubscription = customIntervalObservable.subscribe(
+      (count: number) => {
+        console.log(count);
+      }
+    );
   }
 
   ngOnDestroy(): void {
